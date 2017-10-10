93a4f9be565befccb1c1a2a4df37c6b9cf06121d
Iao Seng Sio <tonysio42@gmail.com>
2017-10-10 10:22:55 -1000

    Task 5: Events, Sessions

--- app/leaderboard.js
+++ app/leaderboard.js
@@ -1,1 +1,28 @@
-console.log("Hello world");
+PlayersList = new Mongo.Collection('players');
+
+if (Meteor.isClient) {
+  Template.leaderboard.helpers({
+    'player': function(){
+      return PlayersList.find();
+    },
+    'selectedClass': function(){
+      var playerId = this._id;
+      var selectedPlayer = Session.get('selectedPlayer');
+      if (playerId === selectedPlayer) {
+        return "selected";
+      }
+    }
+  });
+
+  Template.leaderboard.events({
+    'click .player': function(){
+      var playerId = this._id;
+      Session.set('selectedPlayer', playerId);
+      var selectedPlayer = Session.get('selectedPlayer');
+    }
+  });
+}
+
+if (Meteor.isServer) {
+
+}
